75b5c01d3c13261375728c59bb6bb96b1192cd87
Piotr Dul <debian@localhost.localdomain>
2017-06-26 12:30:46 +0200

    calculator done

--- 1_Zadania/Dzien_1/1_Podstawy_obiektowosci/Calculator.php
+++ 1_Zadania/Dzien_1/1_Podstawy_obiektowosci/Calculator.php
@@ -19,8 +19,53 @@ class Calculator {
     public function getHistory(){
         return $this->history;
     }
+    
+    public function multiply($num1, $num2){
+        $sum = $num1 * $num2;
+        
+        $this->history[] = "multiply $num1 to $num2 got $sum";
+        return $sum;
+    }
+    
+    public function substract($num1, $num2){
+        $sub = $num1 - $num2;
+        
+        $this->history[] = "substract $num1 to $num2 got $sub";
+        return $sub;
+    }
+    
+    public function divide($num1, $num2){
+        $sub = $num1 / $num2;
+        
+        $this->history[] = "divide $num1 to $num2 got $sub";
+        return $sub;
+    }
+    
+    public function printOperations(){
+        foreach ($this->history as $val){
+            echo $val;
+        }
+    }
+    
+    public function clearOperations(){
+        $this->history = [];
+    }
+    
 }
 
 $calc = new Calculator();
+$calc2 = new Calculator();
+
 echo $calc->add(1, 3);
-print_r($calc->getHistory());
+echo $calc->multiply(2, 4);
+
+echo $calc2->substract(5, 2);
+echo $calc2->divide(10, 2);
+echo "<hr>";
+//print_r($calc->getHistory());
+//print_r($calc2->getHistory());
+
+$calc->printOperations();
+echo "<hr>";
+$calc2->printOperations();
+
